db17e24d983d3d1582d3c9f2b03e44dd19fb2fd4
Husam Alhayek <eng.termnh@gmail.com>
2025-06-05 23:57:12 +0300

    renamed anykernel

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -13,7 +13,7 @@ do.cleanuponabort=0
 device.name1=moonstone
 device.name2=stone
 device.name3=sunstone
-supported.versions=12.0-15.0
+supported.versions=
 supported.patchlevels=
 '; } # end properties
 
